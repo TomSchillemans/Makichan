053a79b8448b60e22c91805b1efb527b03764b1f
Tom Schillemans <tomschillemans@gmail.com>
2015-04-01 23:56:06 +0200

    Add devise confirmable to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331222121) do
+ActiveRecord::Schema.define(version: 20150401104221) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
@@ -42,8 +42,12 @@ ActiveRecord::Schema.define(version: 20150331222121) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
